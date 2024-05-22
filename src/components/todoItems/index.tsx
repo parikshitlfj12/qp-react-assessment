017ba8e3b8c955abe2a2bf8db5c19282e8f570ba
Parikshit Singh <parikshitlfj@gmail.com>
2024-05-22 11:34:03 +0530

    Add: Accordion for longer texts

--- src/components/todoItems/index.tsx
+++ src/components/todoItems/index.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Todo } from '../../types';
 import { useTodos } from '../../context/TodoContext';
 import './todoItems.css';
 
 const TodoItem: React.FC<{ todo: Todo }> = React.memo(({ todo }) => {
     const { dispatch } = useTodos();
+    const [isOpen, setIsOpen] = useState(false);
 
     const toggleTodo = () => {
         dispatch({ type: 'TOGGLE_TODO', id: todo.id });
     };
 
+    const toggleAccordion = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <div className="item-container">
-            <input
-                type="checkbox"
-                checked={todo.completed}
-                onChange={toggleTodo}
-            />
-            <span className={`todo-text ${todo.completed ? 'completed' : ''}`}>
-                {todo.text}
-            </span>
+            <div className="accordion-header" onClick={toggleAccordion}>
+                <input
+                    type="checkbox"
+                    checked={todo.completed}
+                    onChange={toggleTodo}
+                    onClick={(e) => e.stopPropagation()}
+                />
+                <span className={`todo-text ${todo.completed ? 'completed' : ''}`}>
+                    {todo.text}
+                </span>
+                <span className="accordion-icon">
+                    {isOpen ? '-' : '+'}
+                </span>
+            </div>
+            {isOpen && (
+                <div className="accordion-content">
+                    <p>{todo.text}</p>
+                </div>
+            )}
         </div>
     );
 });
